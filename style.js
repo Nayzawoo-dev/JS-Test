2d9cfffbf7edc2570098c31ee00fb1bdcdaced42
Nayzawoo-dev <nayzawoo.ace@gmail.com>
2025-04-23 05:46:58 +0630

    JSPratic

--- style.js
+++ style.js
@@ -85,3 +85,16 @@ for(
 ){
     console.log(i + "code block");
 }
+
+const myArray = [
+    { id: 1, name: "Alice", age: 25 },
+    { id: 2, name: "Bob", age: 30 },
+    { id: 3, name: "Charlie", age: 28 }
+  ];
+  
+  for(x of myArray){
+    for(y in x){
+           console.log(x[y]);
+    }
+    console.log("--------------");
+  }
